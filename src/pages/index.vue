31d8897f8c5275b1983403adfebfc426ea9b408f
RB <53470171+RBercasio22@users.noreply.github.com>
2024-09-10 16:04:16 +0800

    Update index.vue
    add network brigde

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,32 @@
 <template>
-  <!-- <HelloWorld /> -->
+  
    <!-- <Login /> -->
+
+  
    
 
 </template>
 
 <script setup>
-  //
+
+ import { ref } from 'vue' 
+ import { netsh } from 'vue'
+ import { ssh } from 'vue'
+
+
+  const ip = ref()
+  const port = ref()
+  const connected = ref()
+  
+function getIp(ip, port){
+  computed(() => {
+     connected.value = netsh.ip
+    
+    
+  })
+  
+}
+  
+  
+
 </script>
